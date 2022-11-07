070b560c9ed5fa8afdbaaafd1777cf04e84c10dc
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2022-11-07 18:53:24 -0300

    Backend project creatd

---
+++ src/main/java/io/github/vinicius1001/catalog/ApiCompletaApplication.java
@@ -0,0 +1,13 @@
+package io.github.vinicius1001.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vinicius1001/catalog/ApiCompletaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vinicius1001.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
